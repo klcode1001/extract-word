79d03a2642773bed5fba945398273f0654d8d4ff
klcode1001 <ledev0726@gmail.com>
2016-05-21 08:42:32 +0900

    Refactoring

--- lib/extract_word.rb
+++ lib/extract_word.rb
@@ -1,27 +1,18 @@
 class ExtractWord
   def execute(file_path)
     text = File.read(file_path)
-    word_sp, processed_text = get_word_sp(text)
-    word = get_word(processed_text)
-    output_result(word_sp, word)
+    word_sp = get_word_sp(text)
+    groups = word_sp.partition{|word, _| word.split(/\s/).size > 1 }
+    output_result(*groups)
   end
 
   private
 
   # 複数の単語で意味をなす英単語を取得するメソッド ex) "Google Play Awards"
   def get_word_sp(text)
-    regex = /[A-Z][\w\/’]*(?:\sof|\s[A-Z][\w\/’]*)+/
-    word_sp = Hash.new(0)
-    processed_text = text.gsub(regex) do |word, hash|
-      word_sp[word] += 1 and ''
-    end
-    [word_sp, processed_text]
-  end
-
-  # 通常の英単語
-  def get_word(text)
-    text.scan(/[\w’-]+/).each_with_object(Hash.new(0)) do |word, hash|
-      hash[word] += 1
+    regex = /(?:[A-Z][\w\/’]*(?:\sof|\s[A-Z][\w\/’]*)+|[\w’-]+)/
+    text.scan(regex).each_with_object(Hash.new(0)) do |word, word_sp|
+      word_sp[word] += 1
     end
   end
 
