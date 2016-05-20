5133c767a05003a2c8c36708c83824bd4ca3e6b3
klcode1001 <ledev0726@gmail.com>
2016-05-21 07:59:49 +0900

    Refactoring

--- lib/extract_word.rb
+++ lib/extract_word.rb
@@ -1,9 +1,9 @@
 class ExtractWord
   def execute(file_path)
     text = File.open(file_path, 'r').read
-    word_sp = get_word_sp(text)
-    array, word = get_word(text, word_sp)
-    output_result(array, word_sp, word)
+    word_sp, processed_text = get_word_sp(text)
+    word = get_word(processed_text)
+    output_result(word_sp, word)
   end
 
   private
@@ -11,28 +11,27 @@ class ExtractWord
   # 複数の単語で意味をなす英単語を取得するメソッド ex) "Google Play Awards"
   def get_word_sp(text)
     regex = /[A-Z][\w\/’]*(?:\sof|\s[A-Z][\w\/’]*)+/
-    text.scan(regex).each_with_object(Hash.new(0)) do |word, hash|
-      hash[word] += 1
+    word_sp = Hash.new(0)
+    processed_text = text.gsub(regex) do |word, hash|
+      word_sp[word] += 1
+      ''
     end
+    [word_sp, processed_text]
   end
 
   # 通常の英単語
-  def get_word(text, word_sp)
-    array = text
-    word_sp.keys.sort_by{|s| s.split(/\s/).size * -1}.each do |word|
-      array = array.gsub("#{word}", "")
-    end
-
+  def get_word(text)
     word = Hash.new(0)
-    array = array.split("\s").map{|m| m.gsub(/[.”,\n“]/, "")}
-    array.each do |w|
+    text = text.split("\s").map{|m| m.gsub(/[.”,\n“]/, "")}
+    text.each do |w|
       word[w] += 1
     end
-    [array, word]
+    word
   end
 
-  def output_result(array, word_sp, word)
-    puts "文字数：#{array.count}"
+  def output_result(word_sp, word)
+    word_count = word.inject(0) {|sum, (_, count)| sum + count}
+    puts "単語数（熟語以外）：#{word_count}"
     puts "英熟語？---------------------------------------------------------------"
     word_sp.sort_by{|_, v| [-v, _]}.each do |word, frequency|
       puts "%3d %s" % [frequency, word]
