674c8c2f8758bdd97ee691935725e011d09c40a2
klcode1001 <ledev0726@gmail.com>
2016-05-21 08:49:46 +0900

    Rename variables and methods

--- lib/extract_word.rb
+++ lib/extract_word.rb
@@ -1,31 +1,31 @@
 class ExtractWord
   def execute(file_path)
     text = File.read(file_path)
-    word_sp = get_word_sp(text)
-    groups = word_sp.partition{|word, _| !word.count(' ').zero? }
-    output_result(*groups)
+    words = count_words(text)
+    single_words, compund_words = words.partition { |word, _| word.count(' ').zero? }
+    output_result(single_words, compund_words)
   end
 
   private
 
-  def get_word_sp(text)
+  def count_words(text)
     regex = /(?:[A-Z][\w\/’]*(?:\sof|\s[A-Z][\w\/’]*)+|[\w’-]+)/
-    text.scan(regex).each_with_object(Hash.new(0)) do |word, word_sp|
-      word_sp[word] += 1
+    text.scan(regex).each_with_object(Hash.new(0)) do |word, count_table|
+      count_table[word] += 1
     end
   end
 
-  def output_result(word_sp, word)
-    word_count = word.inject(0) {|sum, (_, count)| sum + count}
+  def output_result(single_words, compound_words)
+    word_count = single_words.inject(0) {|sum, (_, count)| sum + count}
     puts "単語数（熟語以外）：#{word_count}"
-    output_words(word_sp, '英熟語？')
-    output_words(word, '英単語')
+    output_words(compound_words, '英熟語？')
+    output_words(single_words, '英単語')
   end
 
-  def output_words(word_hash, header)
+  def output_words(count_table, header)
     puts "#{header}------------------------------------------------------------------"
-    word_hash.sort_by{|_, v| [-v, _]}.each do |word, frequency|
-      puts "%3d %s" % [frequency, word]
+    count_table.sort_by{|_, v| [-v, _]}.each do |word, count|
+      puts "%3d %s" % [count, word]
     end
   end
 end
