fc981833627ec4a5b9813054c6c5117ded93566c
klcode1001 <ledev0726@gmail.com>
2016-05-21 08:09:52 +0900

    Refactoring

--- lib/extract_word.rb
+++ lib/extract_word.rb
@@ -28,12 +28,13 @@ class ExtractWord
   def output_result(word_sp, word)
     word_count = word.inject(0) {|sum, (_, count)| sum + count}
     puts "単語数（熟語以外）：#{word_count}"
-    puts "英熟語？---------------------------------------------------------------"
-    word_sp.sort_by{|_, v| [-v, _]}.each do |word, frequency|
-      puts "%3d %s" % [frequency, word]
-    end
-    puts "英単語------------------------------------------------------------------"
-    word.sort_by{|_, v| [-v, _]}.each do |word, frequency|
+    output_words(word_sp, '英熟語？')
+    output_words(word, '英単語')
+  end
+
+  def output_words(word_hash, header)
+    puts "#{header}------------------------------------------------------------------"
+    word_hash.sort_by{|_, v| [-v, _]}.each do |word, frequency|
       puts "%3d %s" % [frequency, word]
     end
   end
